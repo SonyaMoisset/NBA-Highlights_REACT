68a6bbeff75d47991a8b3ba2a29ac3eaf74f408c
smoisset <smoisset@worldremit.com>
2017-09-12 22:44:03 +0100

    Format header & footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
-        <header>
-            <div>Footer</div>
-        </header>
+        <footer>
+            <div className="flexbox-container">
+                <div className="logo">
+                    <span></span>    
+                </div>
+            </div>
+        </footer>
     )
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,14 @@ import { Link } from 'react-router-dom'
 const Header = () => {
     return (
         <header>
-            <div>Header</div>
+            <div className="flexbox-container">
+                <Link to="/" className="logo">
+                    <span></span>    
+                </Link>
+                <nav>
+                    <Link to="/teams">Teams</Link>
+                </nav>
+            </div>
         </header>
     )
 }
