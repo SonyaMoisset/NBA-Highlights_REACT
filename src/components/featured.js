78a1c86d0a8b6e1a84aad3bc2c31fcd9545c07ed
smoisset <smoisset@worldremit.com>
2017-09-13 18:17:57 +0100

    Add slider part 1

---
+++ src/components/featured.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Featured = (props) => {
+
+    return (
+        <div>slider</div>
+    )
+}
+
+export default Featured
--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,36 @@
 import React, {Component} from 'react'
 
+import Featured from './featured'
+
+const URL_HOME = `http://localhost:3004/home`;
 
 class Home extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            home: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch(URL_HOME, {
+            method: "GET"
+        })
+            .then(response => response.json())
+            .then(json => {
+                this.setState({
+                    home: json
+                })   
+        })
+    }
+
     render() {
         return (
-            <div>Home</div>
+            <div>
+                <Featured slides={this.state.home.slider} />    
+            </div>
         )
     }
 }
