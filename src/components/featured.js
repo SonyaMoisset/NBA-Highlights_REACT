5b3b63a8c72dcc17665e24859d448596925b86a5
smoisset <smoisset@worldremit.com>
2017-09-13 18:33:53 +0100

    Add slider part 2

--- src/components/featured.js
+++ src/components/featured.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import Slider from 'react-slick'
+
+const settings = {
+    arrows: false,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+}
+
+const generateSlides = ({slides}) => {
+    if (slides) {
+        return (
+            <Slider {...settings}>
+                {slides.map((slide) => {
+                    return (
+                        <div
+                            key={slide.id}
+                            className="item-slider"
+                            style={{background: `url(/images/covers/${slide.cover})`}}>
+                            <div className="caption">
+                                <h4>{slide.topic}</h4>
+                                <p>{slide.title}</p>
+                            </div>
+                        </div>
+                    )
+               })}
+            </Slider>
+        )
+    }
+}
 
 const Featured = (props) => {
 
     return (
-        <div>slider</div>
+        <div>
+            {generateSlides(props)}    
+        </div>
     )
 }
 
